51c7146da75ec6675eda5f33c53008ae520e5a61
Salva Ivars Suárez <69164969+SalvaIvars@users.noreply.github.com>
2023-02-26 19:52:39 +0100

    FINAL

--- CRUD/Views/Publications/PruebaMapa.xaml.cs
+++ CRUD/Views/Publications/PruebaMapa.xaml.cs
@@ -48,7 +48,7 @@ namespace CRUD.Views.Publications
             string folder = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
             string filter = "*.js";
             string output = folder.Substring(0,folder.IndexOf("CRUD"));
-            output += "CRUDFINAL\\CRUD\\Views\\Publications\\script.js";
+            output += "CRUD\\Views\\Publications\\script.js";
             string[] files = Directory.GetFiles(folder, filter);
             File.AppendAllText(@output, "var flightPlanCoordinates = " + jsonLista + "\r\nvar prueba = [];\r\nfor(let i = 0;  i < flightPlanCoordinates.length; i++){\r\n  prueba[i] = new google.maps.LatLng(flightPlanCoordinates[i][0], flightPlanCoordinates[i][1])\r\n}\r\n loadPosition(prueba[0])\r\nvar flightPath = new google.maps.Polyline({\r\n    map:map,\r\n    path: prueba,\r\n    strokeColor: \"#FF0000\",\r\n    strokeOpacity: 1.0,\r\n    strokeWeight: 2\r\n  })}" + Environment.NewLine);
         }
@@ -59,7 +59,7 @@ namespace CRUD.Views.Publications
             string folder = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName) + @".\";
             string filter = "*.html";
             string output = folder.Substring(0, folder.IndexOf("CRUD"));
-            output += "CRUDFINAL\\CRUD\\Views\\Publications\\index.html";
+            output += "CRUD\\Views\\Publications\\index.html";
             string[] files = Directory.GetFiles(folder, filter);
             Wb.CoreWebView2.Navigate(@output);
         }
@@ -70,7 +70,7 @@ namespace CRUD.Views.Publications
             string filter = "*.js";
             string[] files = Directory.GetFiles(folder, filter);
             string output = folder.Substring(0, folder.IndexOf("CRUD"));
-            output += "CRUDFINAL\\CRUD\\Views\\Publications\\script.js";
+            output += "CRUD\\Views\\Publications\\script.js";
             List<string> quotelist = File.ReadAllLines(@output).ToList();
             for(int i = 36; i > 23; i--)
             {
